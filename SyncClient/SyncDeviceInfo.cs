dbf00a14e7ed477a72ac8d9e2893479e003e02b0
Koen J <git@unkto.com>
2025-04-07 16:00:54 +0200

    Added pairing code.

--- SyncClient/SyncDeviceInfo.cs
+++ SyncClient/SyncDeviceInfo.cs
@@ -13,10 +13,14 @@ public class SyncDeviceInfo
     [JsonPropertyName("port")]
     public int Port { get; set; }
 
-    public SyncDeviceInfo(string publicKey, string[] addresses, int port)
+    [JsonPropertyName("pairingCode")]
+    public string? PairingCode { get; set; }
+
+    public SyncDeviceInfo(string publicKey, string[] addresses, int port, string? pairingCode)
     {
         PublicKey = publicKey;
         Addresses = addresses;
         Port = port;
+        PairingCode = pairingCode;
     }
 }
