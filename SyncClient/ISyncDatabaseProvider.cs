f23f7c55e81e98f6fe1667792bf494e9a2b92c67
Koen J <git@unkto.com>
2025-05-14 12:24:37 +0200

    Integrating SyncSession part 1

---
+++ SyncClient/ISyncDatabaseProvider.cs
@@ -0,0 +1,22 @@
+﻿namespace SyncClient;
+
+public interface ISyncDatabaseProvider
+{
+    // Authorized devices
+    Task<bool> IsAuthorizedAsync(string publicKey);
+    Task AddAuthorizedDeviceAsync(string publicKey);
+    Task RemoveAuthorizedDeviceAsync(string publicKey);
+    Task<string[]> GetAllAuthorizedDevicesAsync();
+
+    // Sync key pair
+    Task<string> GetSyncKeyPairAsync();
+    Task SetSyncKeyPairAsync(string value);
+
+    // Last address storage
+    Task<string> GetLastAddressAsync(string publicKey);
+    Task SetLastAddressAsync(string publicKey, string address);
+
+    // Name storage
+    Task<string> GetDeviceNameAsync(string publicKey);
+    Task SetDeviceNameAsync(string publicKey, string name);
+}
---
+++ SyncClient/SyncDiscoverer.cs
@@ -0,0 +1,11 @@
+﻿namespace SyncClient;
+
+public class SyncDiscoverer
+{
+    private readonly ISyncDatabaseProvider _database;
+
+    public SyncDiscoverer(ISyncDatabaseProvider database)
+    {
+        _database = database;
+    }
+}
